55814d9825e1b5b30c8054fc55bb8679749e632c
Your Name <you@example.com>
2019-07-27 22:46:51 -0700

    Updates

---
+++ precache-manifest.772c968da2a730ac9e17bb48ed8b692d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aacaf22ebac26422d4984df2a13468a",
+    "url": "/memory-react/index.html"
+  },
+  {
+    "revision": "31c0c9a6bf0b17613671",
+    "url": "/memory-react/static/css/main.74fd6b01.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memory-react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "31c0c9a6bf0b17613671",
+    "url": "/memory-react/static/js/main.78e6f99a.chunk.js"
+  },
+  {
+    "revision": "39a3745a50e89c0b0ea4",
+    "url": "/memory-react/static/js/runtime~main.7c494fe2.js"
+  }
+]);
