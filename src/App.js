1064b50cfa50c904871fe8f282747c99b3ef852a
Your Name <you@example.com>
2019-07-27 22:33:33 -0700

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MemCard from "./components/MemCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import mems from "./mem.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.mems to the mems json array
+  state = {
+    mems
+  };
+
+  selectMem = id => {
+    // Filter this.state.mems for mems with an id not equal to the id being removed
+    const mems = this.state.mems.filter(mem => mem.id !== id);
+    // Set this.state.mems equal to the new mems array
+    this.setState({ mems });
+  };
+
+  // Map over this.state.mems and render a memCard component for each mem object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Test Your Memory</Title>
+        {this.state.mems.map(mem => (
+          <MemCard
+            selectMem={this.selectMem}
+            id={mem.id}
+            key={mem.id}
+            name={mem.name}
+            image={mem.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
